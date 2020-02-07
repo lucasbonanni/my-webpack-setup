c847f40088a304f0aab6008f8a420066229cc3a0
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2020-02-07 18:55:23 -0300

    add sass loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './scss/style.scss';
 
 function component() {
   const element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,39 @@
-var path = require('path')
+var path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     plugins: [
-        new CleanWebpackPlugin({
-
-        }), //clean the dist folder
+        new CleanWebpackPlugin({}), //clean the dist folder
         new HtmlWebpackPlugin({
-        title: 'My web page',
-        inject: 'body',
-        filename: 'index.html',
-        template: './src/index.html'
-        }),//generates the html
+            title: 'My web page',
+            inject: 'body',
+            filename: 'index.html',
+            template: './src/index.html'
+        }) //generates the html
     ],
     devServer: {
         contentBase: '/dist'
     },
-
-}
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  'style-loader',
+                  // Translates CSS into CommonJS
+                  'css-loader',
+                  // Compiles Sass to CSS
+                  'sass-loader',
+                ],
+              },
+        ]
+    }
+};
