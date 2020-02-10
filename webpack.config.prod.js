f031012187aad25932688d120dcd4e5043303920
Bonanni <lucas.bonanni@thomsonreuters.com>
2020-02-10 16:19:34 -0300

    output css to dist folder

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,13 +1,21 @@
 var path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.[chunkhash].js'
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCSSAssetsPlugin()
+        ]
     },
     plugins: [
         new CleanWebpackPlugin({}), //clean the dist folder
@@ -15,8 +23,13 @@ module.exports = {
             title: 'My web page',
             inject: 'body',
             filename: 'index.html',
-            template: './src/index.html'
-        }) //generates the html
+            template: './src/index.html',
+        }), //generates the html
+        new MiniCssExtractPlugin({
+            filename: '/css/[name].[contenthash].css',
+            chunkFilename: '/css/[id].[contenthash].css',
+        }),
+        new MinifyPlugin()
     ],
     devServer: {
         contentBase: '/dist'
@@ -27,11 +40,15 @@ module.exports = {
                 test: /\.s[ac]ss$/i,
                 use: [
                     // Creates `style` nodes from JS strings
-                    'style-loader',
+                    MiniCssExtractPlugin.loader,
                     // Translates CSS into CommonJS
                     'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            webpackImporter: true,
+                        },
+                    }
                 ],
                 exclude: [
                     path.resolve(__dirname, "node_modules")
